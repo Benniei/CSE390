f772836d954b83b36851042e608ce1c33bc66e6b
Bennie Chen <Bennie.chen09@gmail.com>
2021-05-28 19:15:45 -0400

    Layout for Shopping List

--- Homework 01/ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ Homework 01/ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,14 +1,25 @@
+/*
+ * CSE 390: Mobile App Development
+ * Name: Bennie Chen
+ * Student ID: 112737201
+ */
+
 package com.example.shoppinglist;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
