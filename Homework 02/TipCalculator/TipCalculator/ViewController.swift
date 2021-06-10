36f46be87a29ecd380fad2911a0216195f648a93
Bennie Chen <Bennie.chen09@gmail.com>
2021-06-09 20:14:49 -0400

    Tip Calculator App Complete

--- Homework 02/TipCalculator/TipCalculator/ViewController.swift
+++ Homework 02/TipCalculator/TipCalculator/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moneyInput: UITextField!
+    @IBOutlet weak var fifteen: UIButton!
+    @IBOutlet weak var eighteen: UIButton!
+    @IBOutlet weak var twentry: UIButton!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var total: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func tipFifteen(_ sender: Any) {
+        let priceString = moneyInput.text
+        let price = Double(priceString!)
+        
+        if let price = price{
+            let tip = price * 0.15
+            let tipOutput = String(format: "Tip Amount: %.2f", tip)
+            tipAmount.text = tipOutput
+            let totalOutput = String(format:"Total: %.2f", (tip + price))
+            total.text = totalOutput
+        }else{
+            tipAmount.text = "INVALID ENTRY"
+            total.text = ""
+        }
+    }
+    
+    
+    @IBAction func tipEighteen(_ sender: Any) {
+        let priceString = moneyInput.text
+        let price = Double(priceString!)
+        
+        if let price = price{
+            let tip = price * 0.18
+            let tipOutput = String(format: "Tip Amount: %.2f", tip)
+            tipAmount.text = tipOutput
+            let totalOutput = String(format:"Total: %.2f", (tip + price))
+            total.text = totalOutput
+        }else{
+            tipAmount.text = "INVALID ENTRY"
+            total.text = ""
+        }
+    }
+    
+    
+    @IBAction func tipTwenty(_ sender: Any) {
+        let priceString = moneyInput.text
+        let price = Double(priceString!)
+        
+        if let price = price{
+            let tip = price * 0.20
+            let tipOutput = String(format: "Tip Amount: %.2f", tip)
+            tipAmount.text = tipOutput
+            let totalOutput = String(format:"Total: %.2f", (tip + price))
+            total.text = totalOutput
+        }else{
+            tipAmount.text = "INVALID ENTRY"
+            total.text = ""
+        }
+    }
 }
 
